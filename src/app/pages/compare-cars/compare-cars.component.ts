a778650e3eb8fe73d3f707be9834c02c1fd21b37
msotomail <msotomail@gmail.com>
2021-07-04 17:30:36 +0200

    compare-cars component

--- src/app/pages/compare-cars/compare-cars.component.ts
+++ src/app/pages/compare-cars/compare-cars.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/interfaces/car.interface';
+import { CarService } from 'src/app/services/car.service';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-compare-cars',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompareCarsComponent implements OnInit {
 
-  constructor() { }
+  cars: Car[];
+  arrSelectedCars: Car[];
+
+  constructor(private carService: CarService, private searchService: SearchService) {
+    this.cars = [];
+    this.arrSelectedCars = [];
+  }
 
   ngOnInit(): void {
+    this.carService.list().subscribe(response => {
+      this.cars = response;
+      this.sortOnAlpha();
+    });
+
+  }
+
+  sortOnAlpha() {
+    let field = 'manufacturer';
+    this.cars.sort((a, b) => (a[field] || "").toString().localeCompare((b[field] || "").toString()));
+  }
+
+  onChange($event) {
+    const selectedCar: Car = this.getSelectedCar($event.target.value);
+    this.arrSelectedCars.push(selectedCar);
+  }
+
+  getSelectedCar(pCar): Car {
+    return this.cars.find(car => car.model === pCar);
+    // return car;
   }
 
   // var numbers = [1, 2, 3, 4, 5];
--- src/app/pages/compare-cars/compare-cars.module.ts
+++ src/app/pages/compare-cars/compare-cars.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompareCarsRoutingModule } from './compare-cars-routing.module';
+import { CompareCarsComponent } from './compare-cars.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CompareCarsComponent],
   imports: [
     CommonModule,
-    CompareCarsRoutingModule
+    CompareCarsRoutingModule,
+    ComponentsModule
   ]
 })
 export class CompareCarsModule { }
